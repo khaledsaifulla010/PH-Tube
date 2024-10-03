559d932259b514baa2b91c01aa76c396fd89cc1b
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-10-03 11:40:37 +0600

    loaded all categories name using button in UI

--- scripts/video.js
+++ scripts/video.js
@@ -1,5 +1,3 @@
-console.log("added");
-
 // Load Categories for load all categories videos  (1)
 const loadCategories = async () => {
   try {
@@ -7,8 +5,8 @@ const loadCategories = async () => {
       "https://openapi.programming-hero.com/api/phero-tube/categories"
     );
 
-    const data = await fetchedData.json();
-    displayCategories(data.categories);
+    const categoriesData = await fetchedData.json();
+    displayCategories(categoriesData.categories);
   } catch (error) {
     console.log("Categories is not Load !");
   }
@@ -16,8 +14,19 @@ const loadCategories = async () => {
 
 // Display Categories for Display all categories videos (2)
 
-const displayCategories = (data) => {
-  console.log(data);
+const displayCategories = (categoriesData) => {
+  //   console.log(categoriesData);
+
+  categoriesData.forEach((singleCategory) => {
+    console.log(singleCategory);
+
+    const button = document.createElement("button");
+    button.classList = "btn";
+    button.innerText = singleCategory.category;
+    const categoriesContainer = document.getElementById("categories");
+
+    categoriesContainer.append(button);
+  });
 };
 
 //  All Functions Call
